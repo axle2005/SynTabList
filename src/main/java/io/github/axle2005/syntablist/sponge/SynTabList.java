909cdf6eb5de8236d91e1027b4eb33f99a7ce847
axle2005 <arnoldryan2005@live.ca>
2017-02-27 21:18:26 -0400

    Remove from tablist works.

--- src/main/java/io/github/axle2005/syntablist/sponge/SynTabList.java
+++ src/main/java/io/github/axle2005/syntablist/sponge/SynTabList.java
@@ -21,6 +21,11 @@ import org.spongepowered.api.plugin.Dependency;
 
 import com.google.inject.Inject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import io.github.axle2005.syntablist.common.PlayerData;
 import io.github.axle2005.syntablist.common.Utils;
 import io.github.axle2005.syntablist.sponge.listeners.ListenerRegister;
@@ -37,6 +42,7 @@ public class SynTabList implements ChannelListener {
 	private ListenerRegister events;
 	private Server server;
 	private GameProfileCache gpmcache;
+	Map<UUID, PlayerData> playersData = new HashMap<>();
 
 	@Listener
 	public void initialization(GameInitializationEvent event) {
@@ -44,7 +50,7 @@ public class SynTabList implements ChannelListener {
 		events = new ListenerRegister(this);
 		server = Sponge.getServer();
 		gpmcache = Sponge.getServer().getGameProfileManager().getCache();
-
+		
 	}
 
 	@Listener
@@ -63,35 +69,53 @@ public class SynTabList implements ChannelListener {
 
 		// Let's get the object from the packet
 		final PlayerData playerData = packet.getObject(PlayerData.class);
-		if (playerData.getType().equals("Quit")) {
-			for (Player player : server.getOnlinePlayers()) {
-				removeTabList(player.getTabList(), playerData.getPlayerUUID());
 
+		if (playerData.getType().equals("Join")) {
+			playersData.put(playerData.getPlayerUUID(), playerData);
+		} else if (playerData.getType().equals("Quit")) {
+			if (playersData.containsKey(playerData.getPlayerUUID())) {
+				playersData.remove(playerData.getPlayerUUID());
 			}
+			log.info(playerData.getPlayerName() + " " + playerData.getPlayerUUID());
+		}
+		for (Player player : server.getOnlinePlayers()) {
 
-		} else if (playerData.getType().equals("Join")) {
-			for (Player player : server.getOnlinePlayers()) {
-				TabList tablist = player.getTabList();
-				tablist.setHeader(
-						Text.of(TextColors.BLUE, "=========", TextColors.WHITE, "DeVco", TextColors.BLUE, "========="));
-				tablist.setFooter(Text.of(TextColors.BLUE, "======================="));
+			TabList tablist = player.getTabList();
 
+			tablist.setHeader(Text.of(TextColors.BLUE, "=======================", Text.NEW_LINE, TextColors.BLUE,
+					"=========", TextColors.WHITE, "DeVco", TextColors.BLUE, "========="));
+			tablist.setFooter(Text.of(TextColors.BLUE, "======================="));
+
+			
+			//Checks if the player has been removed from playersData (Logged out) and removes from tablist
+			if (!playersData.containsKey(playerData.getPlayerUUID())) {
 				Optional<TabListEntry> optional = tablist.getEntry(playerData.getPlayerUUID());
 				if (optional.isPresent()) {
-					TabListEntry entry = optional.get().setDisplayName(Text.of(TextColors.GRAY, sendingServer + " ",
-							TextColors.WHITE, playerData.getPlayerName()));
-					// GameProfile gp =
-					// gpmcache.getById(playerData.getPlayerUUID()).get();
-					// entry =
-					// TabListEntry.builder().displayName(Text.of(TextColors.GRAY,server,TextColors.WHITE,gp.getName().get())).build();
-
-				} else {
-					tablist.addEntry(addTabList(player.getTabList(), playerData.getPlayerUUID(),
-							playerData.getPlayerName(), sendingServer));
+					tablist.removeEntry(playerData.getPlayerUUID());
 				}
-
+				
 			}
+			
+			
+			for (PlayerData pData : playersData.values()) {
+
+				Optional<TabListEntry> optional = tablist.getEntry(pData.getPlayerUUID());
+				if (optional.isPresent()) {
+					TabListEntry entry = optional.get()
+							.setDisplayName(Text.of(TextColors.WHITE, pData.getPlayerName()));
+				} else {
+					tablist.addEntry(addTabList(player.getTabList(), pData.getPlayerUUID(), pData.getPlayerName(),
+							sendingServer));
+				}
+			}
+
 		}
+
+	}
+
+	private void buildTabList(TabList tablist) {
+		tablist.setHeader(Text.of(TextColors.GOLD, "=========DeVco========="));
+		tablist.setFooter(Text.of(TextColors.RED, "======================="));
 
 	}
 
@@ -103,8 +127,9 @@ public class SynTabList implements ChannelListener {
 			gpmcache.add(fakeProfile);
 			gp = gpmcache.getById(uuid);
 		}
+
 		TabListEntry entry = TabListEntry.builder().list(tablist).profile(gp.get()).gameMode(GameModes.SURVIVAL)
-				.displayName(Text.of(TextColors.GRAY, server, TextColors.WHITE, playername)).build();
+				.displayName(Text.of(TextColors.WHITE, playername)).build();
 		return entry;
 
 	}
@@ -112,14 +137,8 @@ public class SynTabList implements ChannelListener {
 	private void removeTabList(TabList tablist, UUID uuid) {
 		Optional<TabListEntry> optional = tablist.getEntry(uuid);
 		if (optional.isPresent()) {
-			tablist.removeEntry(uuid);
+			TabListEntry entry = optional.get();
 		}
-	}
-
-	private void buildTabList(TabList tablist) {
-		tablist.setHeader(Text.of(TextColors.GOLD, "=========DeVco========="));
-		tablist.setFooter(Text.of(TextColors.RED, "======================="));
-
 	}
 
 }
