72fedcfb898ab4b8afeb6abdf9b3e560de718dfa
axle2005 <arnoldryan2005@live.ca>
2017-06-06 16:31:12 -0300

    Removed unused code - Codacy update

--- src/main/java/io/github/axle2005/syntablist/sponge/listeners/ListenerServerStart.java
+++ src/main/java/io/github/axle2005/syntablist/sponge/listeners/ListenerServerStart.java
@@ -5,7 +5,6 @@ import org.spongepowered.api.entity.living.player.Player;
 
 import io.github.axle2005.syntablist.common.ServerData;
 import io.github.axle2005.syntablist.common.Utils;
-import io.github.axle2005.syntablist.sponge.SynTabList;
 import net.kaikk.mc.synx.packets.ChannelListener;
 import net.kaikk.mc.synx.packets.Packet;
 
