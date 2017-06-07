fdc7b919c2174cd0d11805ad481dada90d010da4
axle2005 <arnoldryan2005@live.ca>
2017-06-06 21:06:34 -0300

    Removed several variables.

--- src/main/java/io/github/axle2005/syntablist/sponge/TabListUtil.java
+++ src/main/java/io/github/axle2005/syntablist/sponge/TabListUtil.java
@@ -2,9 +2,6 @@ package io.github.axle2005.syntablist.sponge;
 
 import java.util.Optional;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.gamemode.GameModes;
 import org.spongepowered.api.entity.living.player.tab.TabList;
@@ -17,7 +14,7 @@ public class TabListUtil {
 
     static Text tabHeader;
     static Text tabFooter;
-    
+
     final static GameProfileManager gpm = Sponge.getServer().getGameProfileManager();
 
     public static void removeTabList(TabList tablist, UUID uuid) {
@@ -29,54 +26,31 @@ public class TabListUtil {
     }
 
     public static TabListEntry addTabList(TabList tablist, UUID uuid, Text playername) {
-
-	CompletableFuture<GameProfile> futureGameProfile = gpm.get(uuid);
-	try {
-	    GameProfile gp1 = futureGameProfile.get();
-	    TabListEntry entry = TabListEntry.builder().list(tablist).profile(gp1).gameMode(GameModes.SURVIVAL)
-		    .displayName(playername).build();
-	    return entry;
-	} catch (InterruptedException | ExecutionException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
-	return null;
-
+	return TabListEntry.builder().list(tablist).profile(GameProfile.of(uuid, "")).gameMode(GameModes.SURVIVAL)
+		.displayName(playername).build();
     }
-    
-    public static TabListEntry.Builder addTabList(UUID uuid, Text playername) {
 
-	CompletableFuture<GameProfile> futureGameProfile = gpm.get(uuid);
-	try {
-	    GameProfile gp1 = futureGameProfile.get();
-	    TabListEntry.Builder entry = TabListEntry.builder().profile(gp1).gameMode(GameModes.SURVIVAL)
-		    .displayName(playername);
-	    return entry;
-	} catch (InterruptedException | ExecutionException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
-	return null;
+    public static TabListEntry addFakeEntry(TabList tablist, int fakeUUID) {
 
+	return TabListEntry.builder().list(tablist).latency(0)
+		.profile(GameProfile.of(UUID.fromString("00000000-0000-0000-0000-000000000001"), ""))
+		.gameMode(GameModes.SURVIVAL).displayName(Text.of("")).build();
     }
-    
-    public static TabListEntry addFakeEntry(TabList tablist){
-	return null;
-    }
-    public static void setHeader(Text t){
+
+    public static void setHeader(Text t) {
 	tabHeader = t;
     }
-    public static void setFooter(Text t){
+
+    public static void setFooter(Text t) {
 	tabFooter = t;
     }
-    public static Text getHeader(){
+
+    public static Text getHeader() {
 	return tabHeader;
     }
-    public static Text getFooter(){
+
+    public static Text getFooter() {
 	return tabFooter;
     }
-    
-
-
 
 }
